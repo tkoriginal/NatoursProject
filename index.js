f261eddfb630ad98f3c1ac47a81f26d9d426e591
tkoriginal <tauqueer.khan@gmail.com>
2019-02-18 02:46:16 +0000

    Added app to VPS

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  app.use(express.static('public'));
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('index'))
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
   // .listen(3000,() => console.log('Listening on Port 3000'))
